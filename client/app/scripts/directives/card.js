438796d496d51325a5e22b0d7837fd787dafeacc
Jesper Håkansson <jh222xk@student.lnu.se>
2015-02-05 21:46:38 +0100

    Fixed getting data out to the views

--- client/app/scripts/directives/card.js
+++ client/app/scripts/directives/card.js
@@ -1,25 +1,21 @@
 'use strict';
-
 /**
  * @ngdoc directive
  * @name newsApp.directive:card
  * @description
  * # card
  */
-angular.module('newsApp')
-  .directive('card', ['articles', function (articles) {
-    return {
-      templateUrl: 'partials/card.html',
-      restrict: 'E',
-      controller: function () {
-        var store = this;
+angular.module('newsApp').controller('cardCtrl', ['$scope', 'articles',
+    function($scope, articles) {
         articles.getArticles().success(function(data) {
-          store.articles = data;
+            $scope.articleList = data;
         });
-      },
-      scope: {
-        item: "="
-      },
-      controllerAs: 'cardCtrl'
-    };
-  }]);
+    }
+]).directive('card', ['articles',
+    function(articles) {
+        return {
+            templateUrl: 'partials/card.html',
+            restrict: 'E',
+        };
+    }
+]);
