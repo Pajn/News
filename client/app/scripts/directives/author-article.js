94b4eab392eda05ec193ceb9d951a2513c650e33
th222fa <th222fa@student.lnu.se>
2015-02-05 23:33:47 +0100

    förbättringar

--- client/app/scripts/directives/author-article.js
+++ client/app/scripts/directives/author-article.js
@@ -3,10 +3,8 @@
  */
 
 angular.module('newsApp').controller('AuthorArticlesCtrl', ['$scope', '$routeParams', 'ArticlesByAuthor',
-  function($scope, ArticlesByAuthor, $routeParams) {
-    ArticlesByAuthor.getArticles('2c859005-ad7f-11e4-936a-e124830f2840').success(function(data){
-      console.log('rrfgds');
-      console.log($routeParams.id);
+  function($scope, $routeParams, ArticlesByAuthor) {
+    ArticlesByAuthor.getArticles($routeParams.id).success(function(data){
       $scope.authorItems = data;
       console.log(data);
     })
