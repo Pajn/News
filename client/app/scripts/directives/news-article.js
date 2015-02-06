ec89d2725df10fa96f7a6d03f321bfba74858c58
Jesper Håkansson <jh222xk@student.lnu.se>
2015-02-06 14:26:57 +0100

    FIXED ALL

--- client/app/scripts/directives/news-article.js
+++ client/app/scripts/directives/news-article.js
@@ -7,7 +7,7 @@
  * # newsArticle
  */
 angular.module('newsApp')
-  .controller('newsArticleCtrl', ['$scope', 'articleCategory', '$routeParams', function($scope, articleCategory, $routeParams) {
+  .controller('newsArticleCtrl', ['$scope', 'articleCategory', '$routeParams', 'wikiData', function($scope, articleCategory, $routeParams, wikiData) {
 
     articleCategory.getRelatedArticles($routeParams.id)
       .success(function (data) {
@@ -15,6 +15,25 @@ angular.module('newsApp')
         $scope.articles = data;
         $scope.article = data.article;
         $scope.item = data;
+
+      $scope.wikiData = [];
+      console.log($scope.item.concepts);
+      $scope.item.concepts.forEach(function (con) {
+        if (con.name === 'IS') {
+          con.name = 'Islamiska staten';
+        }
+        console.log(con);
+        wikiData.getWikiData(con.name).success(function(data) {
+          var content = data.parse.text['*'].match(/\<p\>((.|\n)*?)\<\/p\>/gmi)[1];
+          if (typeof content === 'undefined') {
+            return;
+          }
+          content = content.replace(/\<((.|\n)*?)\>/gmi, '');
+          content = content.replace(/\[((.|\n)*?)\]/gmi, '');
+          $scope.wikiData.push({'content': content, 'title': con.name});
+          console.log($scope.wikiData);
+        });
+      });
         //$scope.author = data.author;
       });
 
--- client/app/scripts/directives/wikidata.js
+++ client/app/scripts/directives/wikidata.js
@@ -7,14 +7,7 @@
  */
 angular.module('newsApp').controller('wikiCtrl', ['$scope', 'wikiData',
     function($scope, wikiData) {
-        wikiData.getWikiData('Islamic_State_of_Iraq_and_the_Levant').success(function(data) {
-          console.log($scope.concepts[0].concept.name);
-          var content = data.parse.text['*'].match(/\<p\>((.|\n)*?)\<\/p\>/gmi)[1];
-          content = content.replace(/\<((.|\n)*?)\>/gmi, '');
-          content = content.replace(/\[((.|\n)*?)\]/gmi, '');
-          $scope.wikiData = [content];
-          console.log(content);
-        });
+      
     }
 ]).directive('wiki', function () {
     return {
